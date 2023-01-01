47aa608b2c51938b8b185b1bb98503954ff94ef9
Juan Patten <jrp@maild.name>
2023-01-01 07:25:04 -0800

    checkpoint

--- jif.js
+++ jif.js
@@ -1,13 +1,23 @@
+////////////////////////////////////////////////////////////////////////
+EventTarget.prototype.on = EventTarget.prototype.addEventListener
+EventTarget.prototype.off = EventTarget.prototype.removeEventListener
+////////////////////////////////////////////////////////////////////////
 
-document.designMode = 'on'
+
+document.designMode = 'on' // needed for execCommand
+
+
+const TRIG_TIMEOUT = 1000
 
 
 let ed = document.getElementById('ed')
 
-let api = {}
-let trigs = {}
+let jif = {
+  api: {},
+  trigs: {},
+}
 
-let trigstate = {active:null, timer:null, startpos:null}
+let T = null
 
 
 const keyabbrs = {
@@ -34,12 +44,20 @@ function selection() {
 }
 
 
-function keyrep_str(str) {
-  str = str.trim().toLowerCase()
-  if (str[0] != '<') { return str }
-  let k = str.slice(1, -1).split(/[-.]/)
+function keyrep_str(s) {
+  s = s.trim().toLowerCase()
+  if (s[0] != '<') { return s }
+  let k = s.slice(1, -1).split(/[-.]/)
   let mods = Array.from(new Set(k.slice(0, -1))).sort()
   return [...mods, keyabbr(k.at(-1))].join('.')
+}
+
+function keyrep_evt(e) {
+  let a = e.altKey ? 'a.' : ''
+  let c = e.ctrlKey ? 'c.' : ''
+  let m = e.metaKey ? 'm.' : ''
+  let s = e.shiftKey ? 's.' : ''
+  return a + c + m + s + e.key.toLowerCase()
 }
 
 function keyseq(k) {
@@ -48,50 +66,104 @@ function keyseq(k) {
     .split(/\s+/)
     .map(c => c[0]=='<' ? c : c.split(''))
     .flat()
-    .map(keyrep)
+    .map(keyrep_str)
 }
 
 function addtrig(seq, fn) {
-  let t = trigs
+  let trigs = jif.trigs
   let slen = seq.length
   for (let i=0; i < slen; ++i) {
-    let k = seq[i]
-    if (!t[k]) { t[k] = {f:null, t:{}} }
+    let rep = seq[i]
+    if (!trigs[rep]) { trigs[rep] = {f:null, trigs:{}} }
     if (i == slen-1) {
-      t[k].f = fn
+      trigs[rep].f = fn
     } else {
-      t = t[k].t
+      trigs = trigs[rep].trigs
     }
   }
 }
+
+function cancel_trig() {
+  if (!T) { return }
+  clearTimeout(T.timer)
+  T = null
+}
+
+function finish_trig() {
+  const t = T.active
+  if (t.f) { t.f() }
+  T = null
+}
+
+
+const ignorekeys = ['Alt', 'Control', 'Meta', 'Shift']
+
+ed.on('keydown', e => {
+  if (e.repeat || (e.key in ignorekeys)) { return true }
+
+  const trigs = (T ? T.active : jif).trigs
+  const rep = keyrep_evt(e)
+
+  if (!trigs[rep]) {
+    cancel_trig()
+    return true
+  }
+
+  if (!T) {
+    T = {
+      active: trigs[rep],
+      timer: setTimeout(finish_trig, TRIG_TIMEOUT),
+      startpos: curpos()
+    }
+  } else {
+    clearTimeout(T.timer)
+    T.active = trigs[rep]
+    if (!T.active) {
+      cancel_trig()
+      return true
+    } else {
+      if (Object.keys(T.active.trigs).length) {
+        T.timer = setTimeout(finish_trig, TRIG_TIMEOUT)
+      } else {
+        T.timer = setTimeout(finish_trig, 10)
+      }
+    }
+  }
+  return true
+})
 
 
 ////////////////////////////////////////////////////////////////////////
 
 
-api.key = (key, fn) => {
+jif.api.key = (key, fn) => {
   addtrig(keyseq(key), fn)
 }
 
-api.map = (key, sub) => {
+jif.api.map = (key, sub) => {
   let seq = keyseq(key)
   let pos = curpos()
-  addtrig(seq, () => api.ins(sub, curtrig.start, curpos()))
+  addtrig(seq, () => api.ins(sub, T.startpos, curpos()))
 }
 
-api.ins = (text, bgn, end) => {
+jif.api.ins = (text, bgn, end) => {
   ed.focus()
   if (bgn != null) ed.setSelectionRange(bgn, (end==null ? bgn : end));
   document.execCommand('insertText', false, text)
 }
 
-api.pos = curpos
+jif.api.pos = curpos
 
-api.get = text
+jif.api.get = text
 
 
 ////////////////////////////////////////////////////////////////////////
 
 
-window.api = api
+window.api = jif.api
+
+
+//jif.api.key(';[', () => { console.log("OK!") })
+
+jif.api.map(';[', "foo")
 
